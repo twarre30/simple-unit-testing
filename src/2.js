06ea41eabf152f3811d6fe13a88cd22682800c94
Tiffany Warren <twarre30@gmail.com>
2022-05-05 11:25:44 -0500

    one and two

--- src/2.js
+++ src/2.js
@@ -11,7 +11,7 @@ function whoWon(player1,player2){
     return 'Player 2 wins!';
   }
 
-  if (player1 === 'scissors' && player2 === 'rock '){
+  if (player1 === 'scissors' && player2 === 'rock'){
     return 'Player 2 wins!';
   }
 
